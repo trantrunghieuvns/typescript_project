7b322d0de3e921d2cd957a419ff3de9b351628f5
trantrunghieuvns <trantrunghieuvns@gmail.com>
2022-11-18 11:55:45 +0700

    18.11

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,15 +3,11 @@ import { Link } from 'react-router-dom';
 import { HomePageVideos } from '../Types';
 
 function Card({ data }: { data: HomePageVideos }) {
-
     return (
-
         <div className="flex gap-4 flex-col gap-y-3.5">
             <div className="relative  bg-gray-800">
                 <span className="absolute bottom-3 right-3 text-sm bg-gray-900 px-2 py-0.5 z-10">
-
                     {data.videoDuration}
-
                 </span>
                 <Link to={`/watch/${data.videoId}`} >
                     <img
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { current } from '@reduxjs/toolkit';
 import React, { useEffect } from 'react'
 import InfiniteScroll from 'react-infinite-scroll-component';
 import Card from '../components/Card';
@@ -11,22 +9,24 @@ import { getHomePageVideos } from '../store/reducers/fetchHomePageVideos';
 import { HomePageVideos } from '../Types';
 import '../index.css';
 
+
 export default function Home() {
     const dispatch = useAppDispatch();
+
     const videos = useAppSelector((state) =>
         state.youtubeApp.videos
     );
 
     useEffect(() => {
-        dispatch(getHomePageVideos(false));
+        dispatch(getHomePageVideos(false))
     }, [dispatch])
-
 
     return (
         <div className='max-h-screen overflow-hidden'>
             <div style={{ height: '7.5vh' }}>
                 <Navbar />
             </div>
+
             <div className="flex" style={{ height: '92.5vh' }}>
                 <Sidebar />
 
@@ -34,11 +34,10 @@ export default function Home() {
                     <>
                         <InfiniteScroll //spinner condition
                             dataLength={videos.length}
-                            next={() => dispatch(getHomePageVideos(true))}
+                            next={() => { dispatch(getHomePageVideos(true)) }}
                             hasMore={videos.length < 500}
                             loader={<Spinner />}
-                            height={700}
-
+                            height={670}
                         >
                             <div className="grid md:mt-3 md:gap-y-5 md:gap-x-6 md:grid-cols-3 md:px-6  // lg:gap-y-10 lg:gap-x-4 lg:grid-cols-4 lg:px-4 lg:mt-0 // sm:mt-2 sm:gap-y-6 sm:gap-x-2 sm:grid-cols-2 sm:px-2 // su:mt-4 su:gap-x-4 su:gap-y-6 su:grid-cols-1 su:px-2 ">
                                 {videos.map((item: HomePageVideos) => {
@@ -47,9 +46,8 @@ export default function Home() {
                             </div>
                         </InfiniteScroll>
                     </>
-                ) : <Spinner />}
+                ) : (<Spinner />)}
             </div>
-
         </div>
     )
 }
